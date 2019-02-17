e365434d9691962dff4390f01d397f265781da50
Giacomo Cerreto <frookes@pm.me>
2019-02-17 12:56:10 +0100

    mods

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const pool = mysql.createPool({
 })
 
 app.use(bodyparser.json())
-app.use("*", (req, res, next) => {
+app.all("*", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
