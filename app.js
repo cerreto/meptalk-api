a5516a4fa0c7df27d14854ccbdc4d17b96ce717b
Giacomo Cerreto <frookes@pm.me>
2019-02-18 23:12:01 +0100

    mods

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.options("*", cors())
 app.all("*", (req, res, next) => {
   console.log("cheeeck")
   res.header("Access-Control-Allow-Origin", "*")
-  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
+  res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS, OPTION")
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
   next()
 })
