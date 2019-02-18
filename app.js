2dec248d972e671b317012d53d2a19f7d323923f
Giacomo Cerreto <frookes@pm.me>
2019-02-18 18:28:05 +0100

    mods

--- app.js
+++ app.js
@@ -26,12 +26,12 @@ app.options("*", (req, res) => {
 })
 
 app.use(cors())
-/*app.all("*", (req, res, next) => {
+app.all("*", (req, res, next) => {
   res.header("Access-Control-Allow-Origin", "*")
   res.header("Access-Control-Allow-Methods", "GET, PUT, POST, DELETE, OPTIONS")
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
   next()
-})*/
+})
 
 const authmiddleware = (req, res, next) => {
   const token = req.get("authorization")
