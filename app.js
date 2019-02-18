7a1bc69bcf4bdc86ef5e1d001c21dad051a501ef
Giacomo Cerreto <frookes@pm.me>
2019-02-18 22:18:13 +0100

    mods

--- app.js
+++ app.js
@@ -29,6 +29,7 @@ app.all("*", (req, res, next) => {
   res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, Content-Length, X-Requested-With")
   next()
 })
+app.use(cors())
 app.options("*", (req, res) => {
   res.sendStatus(200)
 })
